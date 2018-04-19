831694af6bd146c3eb4691527f3521552fd7f4c0
Christopher Dunavan <github@hypersprite.com>
2018-04-18 21:38:21 -0700

    updated blog-post and index to match current gatsby-starter-blog and fix page issue, also removed "page" from sample blog posts to match current gatsby-starter-blog and because they were no loger nessary

--- src/pages/2015-05-06-my-second-post/index.md
+++ src/pages/2015-05-06-my-second-post/index.md
@@ -1,7 +1,6 @@
 ---
 title: My Second Post!
 date: "2015-05-06T23:46:37.121Z"
-path: "/my-second-post/"
 ---
 
 Wow! I love blogging so much already.
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -9,17 +9,34 @@ class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+    const { previous, next } = this.props.pathContext
 
     return (
       <div>
         <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} />
         <h1>{post.frontmatter.title}</h1>
-        <p>
-          {post.frontmatter.date}
-        </p>
+        <p>{post.frontmatter.date}</p>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
         <hr />
         <Bio />
+
+        <ul>
+          {previous && (
+            <li>
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            </li>
+          )}
+
+          {next && (
+            <li>
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            </li>
+          )}
+        </ul>
       </div>
     )
   }
@@ -28,14 +45,14 @@ class BlogPostTemplate extends React.Component {
 export default BlogPostTemplate
 
 export const pageQuery = graphql`
-  query BlogPostByPath($path: String!) {
+  query BlogPostBySlug($slug: String!) {
     site {
       siteMetadata {
         title
         author
       }
     }
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
       id
       html
       frontmatter {
